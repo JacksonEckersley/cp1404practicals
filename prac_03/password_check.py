a1e5f8edb3486775dfaa79c889a8305531b7e90a
JacksonEckersley <69130120+JacksonEckersley@users.noreply.github.com>
2021-08-22 11:38:46 +1000

    Refactor password_check.py to use functions.

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,12 +1,23 @@
 
-char_length = 10
-finished = False
-while not finished:
+def main():
+    char_length = 10
+    while True:
+        password = get_password(char_length)
+        if len(password) < char_length:
+            continue
+        else:
+            print_star(password)
+            break
+
+
+def get_password(char_length):
     password = input("Enter a password at least {} characters long: ".format(char_length))
-    if len(password) < char_length:
-        continue
-    else:
-        for i in range(0, len(password)):
-            print("*", end="")
-        finished = True
-        break
+    return password
+
+
+def print_star(password):
+    for i in range(0, len(password)):
+        print("*", end="")
+
+
+main()
