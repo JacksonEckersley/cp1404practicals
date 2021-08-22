e3057b1b96081312cebbd1226e48212f150f8db1
JacksonEckersley <69130120+JacksonEckersley@users.noreply.github.com>
2021-08-22 11:55:36 +1000

    broken_score.py and temperatures.py refactored to use functions.

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -8,14 +8,26 @@ F - Convert Fahrenheit to Celsius
 Q - Quit"""
 print(MENU)
 choice = input(">>> ").upper()
+
+
+def convert_to_farenheit():
+    global fahrenheit
+    fahrenheit = celsius * 9.0 / 5 + 32
+
+
+def convert_to_celcius():
+    global celsius
+    celsius = 5 / 9 * (fahrenheit - 32)
+
+
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
+        convert_to_farenheit()
         print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
+        convert_to_celcius()
         print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
